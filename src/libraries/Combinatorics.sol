b86cdb67fe9e72c2877f8d6bb1087caaf4830da3
mrk-hub <mrkennedy@game7.io>
2025-02-19 11:54:35 -0500

    Added license and pramga

--- src/libraries/Combinatorics.sol
+++ src/libraries/Combinatorics.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
 library Combinatorics {
     function factorial(uint256 n) internal pure returns (uint256 nFactorial) {
         nFactorial = _factorial(1, n);
