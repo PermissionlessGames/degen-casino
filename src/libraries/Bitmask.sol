52a637d82ff9b6ed19f417a4fbaea0ba718344aa
mrk-hub <mrkennedy@game7.io>
2025-04-03 11:20:29 -0400

    Added Test cases for bitmask, Updated Bitmask

--- src/libraries/Bitmask.sol
+++ src/libraries/Bitmask.sol
@@ -4,25 +4,36 @@ pragma solidity ^0.8.19;
 library Bitmask {
     /**
      * @notice Encodes an array of numbers into a single bitmask.
+     * @dev The numbers must be between 0 and 255.
+     * @param numbers The array of numbers to encode.
+     * @return bitmask The encoded bitmask.
      */
     function encodeBitmask(
         uint256[] memory numbers
     ) internal pure returns (uint256 bitmask) {
         for (uint256 i = 0; i < numbers.length; i++) {
-            require(numbers[i] >= 0, "Number out of range");
+            require(
+                numbers[i] >= 0 && numbers[i] <= 255,
+                "Number out of range"
+            );
             bitmask |= (1 << (numbers[i]));
         }
     }
 
     /**
      * @notice Decodes a bitmask into an array of numbers.
+     * @dev The max number must be between 0 and 255. Should be the largest number in the array.
+     * @param bitmask The bitmask to decode.
+     * @param maxNumber The maximum number in the array.
+     * @return numbers The decoded array of numbers.
      */
     function decodeBitmask(
         uint256 bitmask,
         uint256 maxNumber
     ) internal pure returns (uint256[] memory numbers) {
+        require(maxNumber <= 255, "Max number exceeds limit");
         uint256 count;
-        for (uint256 i = 0; i < maxNumber; i++) {
+        for (uint256 i = 0; i <= maxNumber; i++) {
             if ((bitmask & (1 << i)) != 0) {
                 count++;
             }
@@ -30,7 +41,7 @@ library Bitmask {
 
         numbers = new uint256[](count);
         uint256 index = 0;
-        for (uint256 i = 0; i < maxNumber; i++) {
+        for (uint256 i = 0; i <= maxNumber; i++) {
             if ((bitmask & (1 << i)) != 0) {
                 numbers[index++] = i;
             }
@@ -39,6 +50,11 @@ library Bitmask {
 
     /**
      * @notice Counts the number of matching bits between two bitmasks.
+     * @dev The max range must be between 0 and 255.
+     * @param bitmask1 The first bitmask.
+     * @param bitmask2 The second bitmask.
+     * @param maxRange The maximum range to count.
+     * @return count The number of matching bits.
      */
     function countMatchingBits(
         uint256 bitmask1,
@@ -48,9 +64,9 @@ library Bitmask {
         require(maxRange <= 255, "Range exceeds limit"); // Ensure max range is within 255
 
         uint256 count = 0;
-        uint256 diff = bitmask1 & bitmask2; // Identify matching bits
+        uint256 diff = bitmask1 & bitmask2; // Identify matching bits, is a bitmask with only the matching bits
 
-        for (uint8 i = 0; i < maxRange && diff > 0; i++) {
+        for (uint8 i = 0; i <= maxRange && diff > 0; i++) {
             count += diff & 1; // Count the bit if it's set
             diff >>= 1; // Shift right to check the next bit
         }
--- test/Bitmask.t.sol
+++ test/Bitmask.t.sol
@@ -21,6 +21,50 @@ contract BitmaskTest is Test {
         );
     }
 
+    function testEncodeShouldRevertWhenBitsAreLargerThen255() public {
+        //test 255 should pass
+        uint256[] memory numbers = new uint256[](1);
+        numbers[0] = 255;
+        uint256 bitmask = Bitmask.encodeBitmask(numbers);
+        assertEq(bitmask, (1 << numbers[0]));
+        uint256[] memory decodedNumbers = Bitmask.decodeBitmask(bitmask, 255);
+        assertEq(decodedNumbers.length, 1, "Decoded array length is incorrect");
+        assertEq(decodedNumbers[0], 255, "Decoded number is incorrect");
+        //test 256 should revert
+        numbers[0] = 256;
+        vm.expectRevert("Number out of range");
+        bitmask = Bitmask.encodeBitmask(numbers);
+    }
+
+    function testEncode256BitsAndDecode() public pure {
+        uint256[] memory numbers = new uint256[](256);
+        for (uint256 i = 0; i < 256; i++) {
+            numbers[i] = i;
+            assertEq(numbers[i], i, "Number is incorrect");
+        }
+        uint256 bitmask = Bitmask.encodeBitmask(numbers);
+        uint256[] memory decodedNumbers = Bitmask.decodeBitmask(bitmask, 255);
+        assertEq(
+            decodedNumbers.length,
+            numbers.length,
+            "Decoded array length is incorrect"
+        );
+        for (uint256 i = 0; i < decodedNumbers.length; i++) {
+            assertEq(decodedNumbers[i], i, "Decoded number is incorrect");
+            assertEq(numbers[i], decodedNumbers[i], "Number is incorrect");
+        }
+    }
+
+    function testShouldRevertWhenMaxNumberIsLargerThan255() public {
+        uint256 bitmask1 = (1 << 25);
+        vm.expectRevert("Max number exceeds limit");
+        Bitmask.decodeBitmask(bitmask1, 256);
+
+        uint256 bitmask2 = (1 << 24);
+        vm.expectRevert("Range exceeds limit");
+        Bitmask.countMatchingBits(bitmask1, bitmask2, 256);
+    }
+
     function testDecode() public pure {
         uint256 bitmask = (1 << 1) | (1 << 3) | (1 << 4); // Expect [1, 3, 4]
         uint256 maxNumber = 5;
@@ -41,6 +85,24 @@ contract BitmaskTest is Test {
         uint256 bitmask2 = (1 << 1) | (1 << 5) | (1 << 7); // [1, 5, 7]
 
         uint256 expectedMatches = 2; // Matches at positions 1 and 5
+        uint256 actualMatches = Bitmask.countMatchingBits(
+            bitmask1,
+            bitmask2,
+            8
+        );
+
+        assertEq(
+            actualMatches,
+            expectedMatches,
+            "Matching bits count is incorrect"
+        );
+    }
+
+    function testCountMatchingBitsWithZero() public pure {
+        uint256 bitmask1 = 0; // Empty bitmask
+        uint256 bitmask2 = (1 << 1) | (1 << 3) | (1 << 5); // [1, 3, 5]
+
+        uint256 expectedMatches = 0; // No matches since bitmask1 is empty
         uint256 actualMatches = Bitmask.countMatchingBits(
             bitmask1,
             bitmask2,
